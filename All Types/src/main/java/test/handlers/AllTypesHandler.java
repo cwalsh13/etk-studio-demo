3aff03a2868984b8fb431584f2d51b074d137cbb
Claire Walsh <claire.walsh@micropact.com>
2021-02-19 15:37:19 -0500

    update

--- All Types/src/main/java/test/handlers/AllTypesHandler.java
+++ All Types/src/main/java/test/handlers/AllTypesHandler.java
@@ -15,7 +15,7 @@ public class AllTypesHandler implements DataObjectEventHandler {
 
     public void execute(DataObjectEventContext etk) throws ApplicationException {
 
-        etk.getResult().addMessage("Executing event handler from SYSTEM WORKSPACE updated");
+        etk.getResult().addMessage("Executing event handler from SYSTEM WORKSPACE updated again");
     }
 
 }
